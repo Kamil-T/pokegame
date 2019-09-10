58f89a6ec47d38f0c36de1b65f4fdfe7ba857209
Kamil T <trzaskomakamil@gmail.com>
2019-09-10 21:29:28 +0200

    Updates

---
+++ precache-manifest.655c2b84c629cf32ec03f8ec58522546.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d7b29a25a5f8012c40381363988d00",
+    "url": "/pokegame/index.html"
+  },
+  {
+    "revision": "3ea751de62e5c591a47e",
+    "url": "/pokegame/static/css/main.26d7e199.chunk.css"
+  },
+  {
+    "revision": "9ffafaf467452b64506c",
+    "url": "/pokegame/static/js/2.d8a28275.chunk.js"
+  },
+  {
+    "revision": "3ea751de62e5c591a47e",
+    "url": "/pokegame/static/js/main.fc2f8305.chunk.js"
+  },
+  {
+    "revision": "6dd7c629fb4c2c8a4465",
+    "url": "/pokegame/static/js/runtime~main.3f8bc12a.js"
+  }
+]);
