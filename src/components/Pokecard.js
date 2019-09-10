e92fb4f7b2aeed2fa92c0c18b06a8c822fe7dcb8
Kamil T <trzaskomakamil@gmail.com>
2019-09-10 17:05:12 +0200

    Pokedex and PokeGame components added, Game is working

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import s from './Pokecard.module.css'
 
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
 
+let padToThree = number => (number <= 999 ? `00${number}`.slice(-3) : number)
 class Pokecard extends Component {
   render() {
-    let imgSrc = `${POKE_API}${this.props.id}.png`
+    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`
     return (
       <div className={s.pokecard}>
-        <h1>{this.props.name}</h1>
+        <h1 className={s.pokecardTitle}>{this.props.name}</h1>
         <img src={imgSrc} alt={this.props.name} />
-        <div>Type: {this.props.type}</div>
-        <div>Exp: {this.props.exp}</div>
+        <div className={s.pokecardData}>Type: {this.props.type}</div>
+        <div className={s.pokecardData}>Exp: {this.props.exp}</div>
       </div>
     )
   }
